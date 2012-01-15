a28ad85ce292b92039f14f85897995545f7c8eea
Aj00200 <aj00200@aj00200.org>
2012-01-15 13:33:57 -0600

    Added demo config.

--- templates/default.tpl.php
+++ templates/default.tpl.php
@@ -3,6 +3,7 @@
  <head>
   <title><?php $this->eprint($this->title); ?></title>
   <link rel="stylesheet" type="text/css" href="/css/global.css" />
+  <script type="text/javascript" src="/js/global.css"></script>
  </head>
  <body>
    <?php echo($this->body); ?>
