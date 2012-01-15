3d8423941185b989eacfedf2a04d8771b4ca2ba7
Aj00200 <aj00200@aj00200.org>
2012-01-15 13:19:25 -0600

    Base structure

---
+++ templates/default.tpl.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title><?php $this->eprint($this->title); ?></title>
+  <link rel="stylesheet" type="text/css" href="/css/global.css" />
+ </head>
+ <body>
+   <?php echo($this->body); ?>
+ </body>
+</html>
